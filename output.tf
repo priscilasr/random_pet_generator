2ee91e987488a3e39dd91475f69fef6c79d57936
Ash Rajagopal <ash.rajagopal@hashicorp.com>
2023-02-08 16:44:21 -0700

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,29 @@
-# see https://www.terraform.io/docs/language/values/outputs.html
-output "hashitalks_lists" {
-  value = toset([for list in twitter_list.hashitalks : "${var.base_url}${list.uri}"])
+output "a-string" {
+  value       = "example string"
+  description = "The private string of the main server instance."
+}
+
+output "an-int" {
+  value       = 1001
+  description = "The private integer of the main server instance."
+}
+
+output "a-decimal" {
+  value       = 1000.1
+  description = "The private decimal of the main server instance."
+}
+
+output "a-list" {
+  value       = ["example", 1001, 1000.1]
+  description = "The private integer of the main server instance."
+}
+
+output "a-map" {
+  value       = { "example" : 1001, "bar" : 1000.1 }
+  description = "The private integer of the main server instance."
+}
+
+output "a-long-string" {
+  value       = "The private integer of the main server instance is where you want to go when you have the most fun in every Terraform instance you can see in the world that you live in except for dogs because they don't run servers in the same place that humans do."
+  description = "A long string is good for you."
 }
