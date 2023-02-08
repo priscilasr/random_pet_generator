9c7d4b78ad5466713980d9001c3a54321f7c581b
Ash Rajagopal <ash.rajagopal@hashicorp.com>
2023-02-08 14:03:11 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,14 +7,191 @@ terraform {
   }
 }
 
-provider "random" {
-  # Configuration options
+variable "hashitalks" {
+  type = list(object({
+    name        = string,
+    description = string,
+    members     = list(string),
+  }))
+
+  default = [
+    {
+      name        = "HashiTalks: DACH (2021)"
+      description = "Speakers and contributors of HashiTalks: DACH (2021)."
+
+      members = [
+        "cedricfeist",
+        "chfrank_cgn",
+        "daniel-ciaglia",
+        "elft3r",
+        "erikveld",
+        "HansHasselberg",
+        "ksatirli",
+        "lom4r92",
+        "mazin_power",
+        "SoFetchPhoto",
+        "this_is_carsten ",
+        "onlydole",
+      ]
+      }, {
+      name        = "HashiTalks: Deploy (2021)"
+      description = "Speakers and contributors of HashiTalks: Deploy (2021)."
+
+      members = [
+        "AngelOnFira",
+        "attachmentgenie",
+        "griffithsgrant",
+        "i0rek",
+        "JColeMorrison",
+        "joatmon08",
+        "ksatirli",
+        "lockigkind",
+        "majesticmarins",
+        "marcjleblanc",
+        "martinb3",
+        "MNomitch",
+        "proffalken",
+        "roberthTweets",
+        "rustbeltrae",
+        "SoFetchPhoto",
+        "onlydole",
+      ]
+      }, {
+      name        = "HashiTalks: LatAm (2021)"
+      description = "Speakers and contributors of HashiTalks: América Latina (2021)."
+
+      members = [
+        "aminespinoza",
+        "alexisworking",
+        "gnuowned",
+        "herrera_luis10",
+        "joatmon08",
+        "katiereese317",
+        "mariogmd",
+        "patoarvizu",
+        "rmasabela",
+        "yurynino",
+      ]
+      }, {
+      name        = "HashiTalks: Build (2021)"
+      description = "Speakers and contributors of HashiTalks: Build (2021)."
+
+      members = [
+        "_shishir_m",
+        "_spox",
+        "amrtns",
+        "arcdigital",
+        "ASomesan",
+        "bflad",
+        "DSchmidt1992",
+        "devopsjacquie",
+        "enmand",
+        "ericsmalling",
+        "evanphx",
+        "foobly",
+        "HansHasselberg",
+        "herrjulz",
+        "im2nguyen",
+        "JColeMorrison",
+        "jcudit",
+        "joatmon08",
+        "jrasell",
+        "JudithPatudith",
+        "katiereese317",
+        "ksatirli",
+        "kmruddy",
+        "kylepenfound",
+        "marycutrali",
+        "mildwonkey",
+        "mr_paul_banks",
+        "onlydole",
+        "paddycarver",
+        "phillebaba",
+        "pst418",
+        "stmcallister",
+        "sweetpavement",
+        "ThatMightBePaul",
+        "tombuildsstuff",
+        "tr0njavolta",
+        "vlazarenko",
+        "wilkenrivera",
+        "YellsOld",
+      ]
+      }, {
+      name        = "HashiTalks 2022"
+      description = "Speakers and contributors of HashiTalks 2022."
+
+      members = [
+        "AmandaBPerino",
+        "amierchery",
+        "BarrosGuy",
+        "CarlosJuanGP",
+        "ChrisNBlum",
+        "CodeOliveira",
+        "CrivetiMihai",
+        "JBFBELL",
+        "SoFetchPhoto",
+        "TheUnclearSam",
+        "YellsOld",
+        "attachmentgenie",
+        "bradmccoydev",
+        "caffeinepresent",
+        "ciscoconsultant",
+        "c_r_w",
+        "debugagent",
+        "devops_rob",
+        "elifsamedin",
+        "erikveld",
+        "glynn_forrest",
+        "HeddaMeow",
+        "howdevelop",
+        "iaguis",
+        "ingkarinamora",
+        "itsmesumanc",
+        "javajirawat",
+        "JColeMorrison",
+        "joatmon08",
+        "justinklztr",
+        "jrsyo",
+        "kmruddy",
+        "krsiye",
+        "lrpurba",
+        "LucyDavinhart",
+        "marcjleblanc",
+        "martinb3",
+        "mehdilaruelle",
+        "mightymarins",
+        "mrkaran_",
+        "ned1313",
+        "nic972",
+        "onlydole",
+        "owenrum",
+        "patoarvizu",
+        "PilotSchenck",
+        "robertbelson",
+        "rylerhockenbury",
+        "saiyampathak",
+        "sergeypronin",
+        "sheriffjackson",
+        "SolutionGeek",
+        "tracey_pooh",
+        "tracypholmes",
+      ]
+    }
+  ]
 }
 
-resource "random_pet" "pet" {
-  length           = var.word-count
+# see https://registry.terraform.io/providers/paultyng/twitter/latest/docs/resources/list
+resource "twitter_list" "hashitalks" {
+  # see https://www.terraform.io/docs/language/meta-arguments/for_each.html
+  for_each = {
+    for list in var.hashitalks :
+    list.name => list
+  }
+
+  name        = each.value.name
+  mode        = "public"
+  description = each.value.description
+  members     = each.value.members
 }
 
-resource "random_string" "random_word" {
-  length           = var.word-length
-}
